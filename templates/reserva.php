fff0b9b8f9ab6bcf65af1e7aba7571f7e0d70463
ezequiaslb <ezequiaslb@users.noreply.github.com>
2024-01-26 20:32:31 -0300

    Update reserva.php

--- templates/reserva.php
+++ templates/reserva.php
@@ -1,19 +1,7 @@
 <?php
-session_start();
 
-// Verificar se o usuário está autenticado
-if (!isset($_SESSION['usuario_logado'])) {
-    // Redirecionar para a página de login se o usuário não estiver autenticado
-    header('Location: login.php');
-    exit();
-}
+include('../includes/header.php');
+include('../includes/footer.php');
 
-// Verificar se o usuário possui reservas ativas (faça a consulta ao banco de dados aqui)
-
-// Incluir o cabeçalho da página
-
-
-// Incluir o rodapé da página
-include('includes/footer.php');
 ?>
 
